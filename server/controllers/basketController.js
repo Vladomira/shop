8d027e020d72b1e0fad8c229ed9002e2634dc446
Julia_Vladomira <juliavladomira@gmail.com>
2023-03-24 10:35:21 +0100

    get basket data by userId

--- server/controllers/basketController.js
+++ server/controllers/basketController.js
@@ -1,4 +1,5 @@
-const { Basket, BasketDevice } = require("../models/models");
+const { Op } = require("sequelize");
+const { Basket, BasketDevice, Device } = require("../models/models");
 const ApiError = require("../error/ApiError");
 
 class BasketController {
@@ -6,35 +7,66 @@ class BasketController {
       try {
          const { deviceId, userId } = req.body;
          let basketId;
+         console.log("deviceId^userId ", deviceId, userId);
          const basket = await Basket.findOne({ userId });
-
          if (basket) {
             basketId = basket.dataValues.id;
             const findedDevice = await BasketDevice.findOne({
-               basketId,
-               deviceId,
+               where: {
+                  deviceId: deviceId,
+               },
             });
-            if (Number(findedDevice.dataValues.deviceId) === Number(deviceId)) {
-               return next(ApiError.badRequest("Device already in cart"));
+
+            console.log("sdfg ", findedDevice);
+            if (findedDevice) {
+               return (
+                  Number(findedDevice.dataValues.deviceId) ===
+                     Number(deviceId) &&
+                  next(ApiError.badRequest("Device already in cart"))
+               );
+            } else if (!findedDevice) {
+               const basketDevice = await BasketDevice.create({
+                  basketId,
+                  deviceId,
+               });
+               return res.json(basketDevice);
             }
          }
          if (!basket) {
             const { dataValues } = await Basket.create({ userId });
             basketId = dataValues.id;
+            const basketDevice = await BasketDevice.create({
+               basketId,
+               deviceId,
+            });
+            return res.json(basketDevice);
          }
-         const basketDevice = await BasketDevice.create({ basketId, deviceId });
-         return res.json(basketDevice);
       } catch (error) {
          return next(ApiError.badRequest(error.message));
       }
    }
-   async getBasketDevices(req, res, next) {
+   async getBasketDeviceById(req, res, next) {
       try {
          const { id } = req.params;
          const userBasket = await Basket.findOne({ id });
          const basketId = userBasket.dataValues.id;
          const basketDevices = await BasketDevice.findAll({ basketId });
          return res.json(basketDevices);
+      } catch (error) {
+         return next(ApiError.badRequest(error.message));
+      }
+   }
+   async getBasketDevicesInfo(req, res, next) {
+      const { userId } = req.params;
+      try {
+         const userBasket = await Basket.findOne({ userId });
+         const devicesIds = await BasketDevice.findAll({
+            where: { basketId: userBasket.dataValues.id },
+         }).then((data) => data.map((el) => el.deviceId));
+         const devicesInfo = await Device.findAll({
+            where: { id: devicesIds },
+         });
+         return res.json(devicesInfo);
       } catch (error) {
          return next(ApiError.badRequest(error.message));
       }
--- server/routes/basketRouter.js
+++ server/routes/basketRouter.js
@@ -4,7 +4,8 @@ const BasketController = require("../controllers/BasketController");
 const authMidlware = require("../midlware/authMidlware");
 
 router.post("/", authMidlware, BasketController.create);
-router.get("/:id", authMidlware, BasketController.getBasketDevices);
+router.get("/:id", authMidlware, BasketController.getBasketDevicesInfo);
+router.get("/:id", authMidlware, BasketController.getBasketDeviceById);
 
 // **
 // router.delete("/");
